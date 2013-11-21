5ec0545ee10570cf0531dcbf7e4b0ff3608c6dbc
Matthew Roselli <matt.roselli15@gmail.com>
2013-11-20 22:32:59 -0600

    Moved activity package/directory structure

--- src/com/example/android/accelerometerplay/AccelerometerPlayActivity.java
+++ src/com/example/android/accelerometerplay/AccelerometerPlayActivity.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.example.marblegame;
+package com.example.android.accelerometerplay;
 
 import android.app.Activity;
 import android.content.Context;
